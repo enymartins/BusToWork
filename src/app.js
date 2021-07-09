e293fbe7ab0e5263ba88e79bd82fc49044f7c6ae
Eny Martins <enyalvesmartins@gmail.com>
2021-07-08 20:31:12 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+const cors = require('cors');
 const app = express();
 
 const db = require('../src/data/database');
 db.connect();
+
+app.use(cors())
 app.use(express.json());
 
 const index = require('./routes/index')
